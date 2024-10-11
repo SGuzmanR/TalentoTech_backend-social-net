7ae74aac67c53ac8ad80f579dd1260d23623d4d3
SGuzmanR <sguzmanr2@gmail.com>
2024-10-11 10:20:31 -0500

    Modified user.js in controllers and routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,31 @@
+// Metodo de prueba del controlador user
 export const testUser = (req, res) => {
   return res.status(200).send({
     message: "Mensaje enviado desde el controlador de Usuarios"
   });
+};
+
+// Metodos Registro de Usuarios
+export const register = (req, res) => {
+  try {
+    // Obtener los datos de la peticion
+    let params = req.body;
+
+    // Validar los datos obtenidos
+
+    // Cifrar la contraseña
+
+    // Devolver el usuario registrado
+    return res.status(200).json({
+      message: "Registro de usuario exitoso",
+      params
+    });
+
+  } catch (error) {
+    console.log("", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error en el Registro de usuarios"
+    });
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { testUser } from "../controllers/user.js"
+import { testUser, register } from "../controllers/user.js"
 
 const router = Router();
 
 // Definir rutas de User
 router.get('/test-user', testUser);
+router.get('/register', register);
 
 export default router;
